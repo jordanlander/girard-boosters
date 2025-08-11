956bf5a25e90de3c90717b0ca12248df14776187
Jordan Lander <jordanlander@gmail.com>
2025-08-10 20:01:01 -0400

    deploy: replace Next with static index.html

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+﻿/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/girard-boosters' : '',
+  assetPrefix: isProd ? '/girard-boosters/' : '',
+};
+export default nextConfig;
